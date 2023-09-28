e10eb113ba126f448ab0a041e09b826742b8b0a8
dlbrptrvc <37641298+dlbrptrvc@users.noreply.github.com>
2023-09-28 19:30:39 +0200

    UI added.

--- script.js
+++ script.js
@@ -4,64 +4,36 @@ const scissors = {value:2,name:'Scissors'}
 const options = [rock, paper, scissors]
 let score = [0,0]
 
+let scoreBoard = document.querySelector('#scoreBoard')
+let optionBtns = document.querySelectorAll('[id^="option"]')
+optionBtns.forEach( (opt) => opt.addEventListener('click',() => showScore(playRound(options[+(opt.id.split('_')[1])]))))
+
+function showScore(roundResult) {
+    scoreBoard.innerHTML = roundResult+"<br>SCORE<br>You:"+score[0]+"<br>MCP:"+score[1]+"<br>"
+    if (score[0]==5) {
+        scoreBoard.innerHTML += "You win!"
+        score = [0,0]
+    }
+    if (score[1]==5) {
+        scoreBoard.innerHTML += "MCP wins!"
+        score = [0,0]
+    }   
+}
+
 function getComputerChoice() {
     return options[Math.floor(Math.random()*3)]
 }
 
 function playRound(userChoice) {
+    // console.log(userChoice)
     let computerChoice = getComputerChoice()
-    userChoice = processUserInput(userChoice)
     if ((userChoice.value+1)%3 == computerChoice.value) {
         score[1]++
         return 'You lose. '+computerChoice.name+' beats '+userChoice.name+'.'
     } else if (userChoice.value == computerChoice.value) {
-        score[0]+=0.5 , score[1]+=0.5
         return 'Draw.'
     } else {
         score[0]++
         return 'You win. '+userChoice.name+' beats '+computerChoice.name+'.'
     }
-}
-
-function game() {
-    alert('Rock,paper,scissors? Best out of 5? Game on!')
-    let round = 0
-    let userChoice = ''
-    while (score[1]<3 && score[0]<3 && round<5) {
-        round++
-        userChoice = prompt('Round '+round+'.\nyou: '+score[0]+'\nme: '+score[1]+'\nChoose your weapon.')
-        if (userChoice == null) {
-            round--
-            switch (round) {
-                case 1:
-                    alert('Running away ey? Just so you know the score after 1 round was\nyou: '+score[0]+'\nme: '+score[1])
-                    break
-                default:
-                    alert('Running away ey? Just so you know the score after '+round+
-                    ' rounds was You: '+score[0]+', me: '+score[1])
-                    round = 0
-                    return
-            }
-        } else if (processUserInput(userChoice)[0]=='I') {
-            round--
-            alert('Are you sure you know how to play this?')
-        }
-        else {
-            alert(playRound(userChoice))
-        }
-    }
-    if (score[0]>score[1]) {alert('Congratulations. You win. The final score is\nyou: '+score[0]+'\nme:'+score[1]+"\nCare for a rematch?")}
-    if (score[0]<score[1]) {alert('Ha, Ha! You lose. The final score is\nyou: '+score[0]+'\nme:'+score[1]+"\nCare for a rematch?")}
-    if (score[0]==score[1]) {alert("I guess it's a draw. The final score is\nyou: "+score[0]+"\nme:"+score[1]+"\nCare for a rematch?")}
-}
-
-function processUserInput(userChoice) {
-    if (userChoice[0] && userChoice.slice(1) && typeof(userChoice)=='string') {
-        for (let i=0;i<3;i++) {
-            if (options[i].name == (userChoice[0].toUpperCase() + userChoice.slice(1).toLowerCase())) {
-                return options[i]
-             }
-        }
-    }
-    return 'Invalid input. Try rock, paper or scissors'
 }
